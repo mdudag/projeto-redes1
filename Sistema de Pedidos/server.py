56e0164a2b7aa9402672a4eca2978575ea4ab1d7
ThallesCerqueira <thalles.cerqueira@cepedi.org.br>
2024-12-09 19:01:29 -0300

    Melhorias no arquivo server

--- Sistema de Pedidos/server.py
+++ Sistema de Pedidos/server.py
@@ -5,118 +5,116 @@ import json
 # Classe que implementa o servidor da padaria
 class ServidorPadaria:
     
-    # Construtor do servidor, inicializa a configuração da rede e variáveis internas
     def __init__(self, host='127.0.0.1', porta=65432):
-        # Criação de um socket TCP/IP
-        self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # Bind do socket ao endereço e porta fornecidos
-        self.socket_servidor.bind((host, porta))
-        
-        # Coloca o servidor em modo de escuta para até 5 conexões simultâneas
-        self.socket_servidor.listen(5)
-        
-        # Exibe uma mensagem indicando que o servidor está online
+        """
+        Inicializa o servidor, configurando o socket e variáveis internas.
+        """
+        self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Criação do socket TCP/IP
+        self.socket_servidor.bind((host, porta))  # Bind do socket ao endereço e porta fornecidos
+        self.socket_servidor.listen(5)  # Modo de escuta, permitindo até 5 conexões simultâneas
         print(f"\nServidor online em {host}:{porta}")
-
-        # Lista para armazenar os pedidos recebidos
-        self.pedidos = []
         
-        # Dicionário para armazenar os clientes registrados (cozinha e área de prontos)
-        self.clientes = {"kitchen": None, "ready_area": None}
-        
-        # Lock para garantir acesso seguro a recursos compartilhados entre threads
-        self.bloqueio = threading.Lock()
+        self.pedidos = []  # Lista para armazenar os pedidos recebidos
+        self.clientes = {"kitchen": None, "ready_area": None}  # Dicionário de clientes registrados
+        self.bloqueio = threading.Lock()  # Lock para acesso seguro a recursos compartilhados
 
-    # Função que trata a comunicação com cada cliente
     def tratar_cliente(self, conn, endereco):
+        """
+        Trata a comunicação com cada cliente (cozinha ou área de prontos).
+        """
         print(f"\nConectado à {endereco}")
         try:
-            # Loop contínuo para receber dados do cliente
             while True:
-                # Recebe os dados enviados pelo cliente
-                dados = conn.recv(1024).decode('utf-8')
-                
-                # Se não houver dados, encerra a conexão
+                dados = conn.recv(1024).decode('utf-8')  # Recebe dados do cliente
                 if not dados:
-                    break
+                    break  # Se não houver dados, encerra a conexão
                 
                 # Converte os dados recebidos de JSON para dicionário Python
                 mensagem = json.loads(dados)
-
-                # Processar o registro de novos clientes (cozinha ou área de prontos)
-                if mensagem['type'] == 'register':
-                    # Registra o cliente no dicionário
-                    self.clientes[mensagem['role']] = conn
-                    print(f"{mensagem['role']} registrado.")
-
-                # Processar novos pedidos do totem
-                elif mensagem['type'] == 'new_order':
-                    # Uso do lock para garantir que a lista de pedidos seja acessada de forma segura
-                    with self.bloqueio:
-                        # Cria um novo pedido com um ID único e status "preparando"
-                        id_pedido = len(self.pedidos) + 1
-                        pedido = {'id': id_pedido, 'item': mensagem['item'], 'status': 'preparando'}
-                        
-                        # Adiciona o pedido à lista de pedidos
-                        self.pedidos.append(pedido)
-                        print(f"\nNovo pedido recebido: {pedido}")
-                    
-                    # Se a cozinha estiver registrada, notifica-a sobre o novo pedido
-                    if self.clientes["kitchen"]:
-                        self.clientes["kitchen"].send(json.dumps({'type': 'new_order', 'order': pedido}).encode('utf-8'))
-
-                # Atualizar o status de um pedido na cozinha concluído
-                elif mensagem['type'] == 'update_order':
-                    # Uso do lock para garantir que a lista de pedidos seja acessada de forma segura
-                    with self.bloqueio:
-                        # Busca o pedido pelo ID e atualiza o status para "pronto"
-                        for pedido in self.pedidos:
-                            if pedido['id'] == mensagem['order_id']:
-                                pedido['status'] = 'pronto'
-                                print(f"Pedido {mensagem['order_id']} marcado como pronto")
-                                break
-                    
-                    # Notifica a área de prontos sobre a atualização do status
-                    if self.clientes["ready_area"]:
-                        self.clientes["ready_area"].send(json.dumps({'type': 'order_ready', 'order_id': mensagem['order_id']}).encode('utf-8'))
-                
-                # Atualizar o status de um pedido na cozinha cancelado
-                elif mensagem['type'] == 'canceled_order':
-                    # Uso do lock para garantir que a lista de pedidos seja acessada de forma segura
-                    with self.bloqueio:
-                        # Busca o pedido pelo ID e atualiza o status para "pronto"
-                        for pedido in self.pedidos:
-                            if pedido['id'] == mensagem['order_id']:
-                                pedido['status'] = 'pronto'
-                                print(f"Pedido {mensagem['order_id']} marcado como cancelado")
-                                break
-
-                # Enviar a lista de pedidos para o cliente (área de prontos ou cozinha)
-                elif mensagem['type'] == 'get_orders':
-                    conn.send(json.dumps({'type': 'orders', 'orders': self.pedidos}).encode('utf-8'))
+                self.processar_mensagem(mensagem, conn)
         except Exception as e:
-            # Em caso de erro, exibe a mensagem de erro
             print(f"Erro: {e}")
         finally:
-            # Fecha a conexão com o cliente
-            conn.close()
+            conn.close()  # Fecha a conexão com o cliente
 
-    # Função para iniciar o servidor e esperar por conexões
+    def processar_mensagem(self, mensagem, conn):
+        """
+        Processa a mensagem recebida do cliente e executa a ação apropriada.
+        """
+        tipo = mensagem['type']
+
+        if tipo == 'register':
+            self.registrar_cliente(mensagem)
+        elif tipo == 'new_order':
+            self.novo_pedido(mensagem)
+        elif tipo == 'update_order':
+            self.atualizar_pedido(mensagem)
+        elif tipo == 'canceled_order':
+            self.cancelar_pedido(mensagem)
+        elif tipo == 'get_orders':
+            self.enviar_pedidos(conn)
+
+    def registrar_cliente(self, mensagem):
+        """
+        Registra um novo cliente (cozinha ou área de prontos).
+        """
+        self.clientes[mensagem['role']] = conn
+        print(f"{mensagem['role']} registrado.")
+
+    def novo_pedido(self, mensagem):
+        """
+        Processa um novo pedido e envia para a cozinha, se estiver registrada.
+        """
+        with self.bloqueio:
+            id_pedido = len(self.pedidos) + 1
+            pedido = {'id': id_pedido, 'item': mensagem['item'], 'status': 'preparando'}
+            self.pedidos.append(pedido)
+            print(f"\nNovo pedido recebido: {pedido}")
+        
+        if self.clientes["kitchen"]:
+            self.clientes["kitchen"].send(json.dumps({'type': 'new_order', 'order': pedido}).encode('utf-8'))
+
+    def atualizar_pedido(self, mensagem):
+        """
+        Atualiza o status de um pedido para "pronto" e notifica a área de prontos.
+        """
+        with self.bloqueio:
+            for pedido in self.pedidos:
+                if pedido['id'] == mensagem['order_id']:
+                    pedido['status'] = 'pronto'
+                    print(f"Pedido {mensagem['order_id']} marcado como pronto")
+                    break
+        
+        if self.clientes["ready_area"]:
+            self.clientes["ready_area"].send(json.dumps({'type': 'order_ready', 'order_id': mensagem['order_id']}).encode('utf-8'))
+
+    def cancelar_pedido(self, mensagem):
+        """
+        Marca um pedido como "cancelado".
+        """
+        with self.bloqueio:
+            for pedido in self.pedidos:
+                if pedido['id'] == mensagem['order_id']:
+                    pedido['status'] = 'cancelado'
+                    print(f"Pedido {mensagem['order_id']} marcado como cancelado")
+                    break
+
+    def enviar_pedidos(self, conn):
+        """
+        Envia a lista de pedidos para o cliente.
+        """
+        conn.send(json.dumps({'type': 'orders', 'orders': self.pedidos}).encode('utf-8'))
+
     def iniciar(self):
+        """
+        Inicia o servidor e espera por conexões.
+        """
         print("Aguardando por conexões...")
-        # Loop contínuo para aceitar conexões de clientes
         while True:
-            # Aceita uma nova conexão de um cliente
-            conn, endereco = self.socket_servidor.accept()
-            # Cria uma nova thread para tratar o cliente de forma independente
-            threading.Thread(target=self.tratar_cliente, args=(conn, endereco)).start()
-
+            conn, endereco = self.socket_servidor.accept()  # Aceita nova conexão
+            threading.Thread(target=self.tratar_cliente, args=(conn, endereco)).start()  # Cria nova thread para tratar o cliente
 
 # Bloco principal que executa o servidor
 if __name__ == "__main__":
-    # Cria uma instância do servidor da padaria
     servidor = ServidorPadaria()
-    
-    # Inicia o servidor
     servidor.iniciar()
