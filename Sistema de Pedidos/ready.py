0806a8aaeaa20930a69bd93b9e34a441f1479448
Duda <106630667+mdudag@users.noreply.github.com>
2024-12-09 23:39:28 -0300

    Update ready.py

--- Sistema de Pedidos/ready.py
+++ Sistema de Pedidos/ready.py
@@ -1,5 +1,5 @@
-import socket   # Importando módulo socket para criar a comunicação de rede entre cliente e servidor
-import json     # Importando módulo JSON para trabalhar com dados no formato JavaScript Object Notation
+import socket   
+import json     
 
 def ready_area_client():
     """
@@ -7,14 +7,13 @@ def ready_area_client():
         do pedido finalizado, e o "cliente totem de prontos" mostra uma mensagem informando o número do pedido.
     """
 
-    host = '127.0.0.1'  # Endereço IP local utilizado para se conectar
-    port = 65432        # Porta arbitrária para estabelecer uma conexão entre o cliente e o servidor
+    # Endereço para se comunicar ao servidor
+    host = '127.0.0.1'  
+    port = 65432        
 
     print("\n+========== TOTEM DE PEDIDOS PRONTOS ==========+")
 
-    # Criando objeto socket com dois parâmentros
-        # AF_INET: define que o IPv4 é o tipo de endereço de rede utilizado
-        # SOCK_STREAM: define que o TCP é o tipo de protocolo utilizado
+    # Criando objeto socket com tipo de endereço de rede IPv4 e protocolo TCP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         # Método de conexão para o cliente se conectar ao servidor
@@ -22,18 +21,18 @@ def ready_area_client():
         print("\n Conectado ao Servidor")
         print("\n ------------- Pedidos Finalizados ------------\n")
 
-        # Registra no servidor como área de pedidos prontos
+        # Envio da mensagem que registra no servidor como área de pedidos prontos
         register_message = {'type': 'register', 'role': 'ready_area'}
-        s.send(json.dumps(register_message).encode('utf-8'))    # Envia a mensagem codificada ao servidor
+        s.send(json.dumps(register_message).encode('utf-8'))
 
         while True:
-            # 1024: especifica o tamanho máximo do dado, em bytes, que será recebido por vez
-            data = s.recv(1024).decode('utf-8') # Recebendo os dados do servidor e decodificando de bytes para string
-            message = json.loads(data)  # Convertendo os dados em string JSON em objeto python
+            data = s.recv(1024).decode('utf-8')
+            message = json.loads(data)
 
-            if message['type'] == 'order_ready':    # Se for uma mensagem de pedido pronto
+            # Mensagem do tipo pedido pronto
+            if message['type'] == 'order_ready':
                 print(f"\t     * Pedido de número {message['order_id']} *")
 
 
-if __name__ == "__main__":  # Se o arquivo for executado diretamente
-    ready_area_client()     # Chama função para executar o totem dos pedidos dos clientes
+if __name__ == "__main__":
+    ready_area_client()
